89035f3708549f68ee70b8b7ac461e7b3dc8d474
timdogfr <103558704+timdogfr@users.noreply.github.com>
2022-05-19 17:25:07 +0300

    Update Roadmap.js

--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -34,32 +34,36 @@ function Roadmap() {
                     <RoadmapItem n='Phase 0' icon={<SettingsIcon color="white" fontSize="32px" />}>
                         <Text fontSize="lg" fontStyle="italic"><Badge colorScheme='purple' p={2} px={4} rounded={10}>2022 Q2</Badge></Text>
                         <Text>• Website Publishing </Text>
-                        <Text>• Giveaway Programmes for Grow the Community</Text>
-                        <Text>• Mint NFTs</Text>
+                        <Text>• Social Media Publishing </Text>
+                        <Text>• Giveaway Programmes for Grow the Community </Text>
                     </RoadmapItem>
 
                     <RoadmapItem n="Phase 1" icon={<CalendarIcon color="white" fontSize="32px" />}>
                         <Text fontSize="lg" fontStyle="italic"><Badge colorScheme='purple' p={2} px={4} rounded={10}>2022 Q3</Badge></Text>
-                        <Text>• NFT Marketplace Listings</Text>
-                        <Text>• Publish New Erectus on Other Blockchains </Text>
-                        <Text>• Publish Our 2nd Evolution Serie on Celo Network (Celo ??????) </Text>
+                        <Text>• Funky Shaker Utility Token Launch </Text>
+                        <Text>• Funky World Game Design Introduction </Text>
+                        <Text>• Funky World Game Asset Design Introduction </Text>
+                        <Text>• Funky World Game Mechanics Introduction </Text>
                     </RoadmapItem>
 
                     <RoadmapItem n="Phase 2" icon={<TimeIcon color="white" fontSize="32px" />}>
                         <Text fontSize="lg" fontStyle="italic"><Badge colorScheme='purple' p={2} px={4} rounded={10}>2022 Q4</Badge></Text>
-                        <Text>• Publish Our 3rd Evolution Serie on Celo Network and 2nd Evolution Serie on Other Blockchains </Text>
+                        <Text>• Funky Avatars Equipment Introduction </Text>
+                        <Text>• Funky World Ranking System And Reward System Introduction </Text>
+                        <Text>• Funky World Game Sneakpeeks </Text>
                     </RoadmapItem>
 
                     <RoadmapItem n="Phase 3" icon={<StarIcon color="white" fontSize="32px" />} >
                         <Text fontSize="lg" fontStyle="italic"><Badge colorScheme='purple' p={2} px={4} rounded={10}>2023 Q1</Badge></Text>
-                        <Text>• Publish Our 4th Evolution Serie on Celo Network </Text>
-                        <Text>• Publish Our 3rd and 4th Evolution Series on Other Blockchanis </Text>
-                        <Text>• Open Evolution Market which you can buy physically decorative evolution objects on our market with the nfts you hold. </Text>
+                        <Text>• Funky World Gameplay Trailer Preimere </Text>
+                        <Text>• Funky World Beta Version Launch </Text>
+                        <Text>• Funky Merch Launch </Text>
                     </RoadmapItem>
 
                     <RoadmapItem n="Phase 4" icon={<CheckCircleIcon color="white" fontSize="32px" />} >
                         <Text fontSize="lg" fontStyle="italic"><Badge colorScheme='purple' p={2} px={4} rounded={10}>2023 Q2</Badge></Text>
-                        <Text>• We continue for creating new Evolution Series and making updates to increase engagement in the market.</Text>
+                        <Text>• Investor And Partnerships Announcement </Text>
+                        <Text>• Coming SOON .. </Text>
                     </RoadmapItem>
                 </VStack>
             </Box>
